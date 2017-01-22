a64070691d06cdb475716f56f7877bf2b0f41308
Makoto Nishijima <nishijima@gmail.com>
2017-01-22 23:05:46 +0900

    機能拡張編第７章 顧客のアカウント編集の確認画面の機能まで完了

--- spec/features/customer/account_management_spec.rb
+++ spec/features/customer/account_management_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 feature '顧客によるアカウント管理' do
-  include FeatureSpecHelper
+  include FeaturesSpecHelper
 
   given(:customer) { create(:customer) }
 
@@ -14,9 +14,8 @@ feature '顧客によるアカウント管理' do
 
   scenario '顧客が基本情報、自宅住所、勤務先を更新する' do
     within('fieldset#customer-fields') do
-      fill_in 'メールアドレス', with: 'songoku@dragon.ball'
       fill_in 'form_customer_family_name', with: '孫'
-      fill_in 'form_customer_given_name', with: '悟空'
+      fill_in 'form_customer_given_name', with: '悟飯'
       fill_in 'form_customer_family_name_kana', with: 'そん'
       fill_in 'form_customer_given_name_kana', with: 'ゴクウ'
       fill_in '生年月日', with: '2001-04-01'
@@ -30,7 +29,26 @@ feature '顧客によるアカウント管理' do
     end
     click_button '確認する'
     click_button '訂正'
-
+    fill_in 'form_customer_given_name', with: '悟空'
+    within('fieldset#work-address-fields') do
+      fill_in '会社名', with: '西島Web製作所'
     end
+    click_button '確認する'
+    click_button '更新'
+
+    customer.reload
+    expect(customer.family_name).to eq '孫'
+    expect(customer.given_name).to eq '悟空'
+    expect(customer.family_name_kana).to eq 'ソン'
+    expect(customer.given_name_kana).to eq 'ゴクウ'
+    expect(customer.birthday).to eq Date.new(2001, 4, 1)
+    expect(customer.personal_phones[0].number).to eq '0311112222'
+    expect(customer.personal_phones[0].primary).to be_truthy
+    expect(customer.personal_phones[1].number).to eq '0755001234'
+    expect(customer.personal_phones[1].primary).to be_falsey
+    expect(customer.home_address.postal_code).to eq '1234567'
+    expect(customer.home_address.phones[0].number).to eq '0698761234'
+    expect(customer.work_address.company_name).to eq '西島Web製作所'
+
   end
 end
